8adae712c5da9318edd54b98a8d1efafbdf33155
Adijat <adijat.buari@yahoo.com>
2019-12-28 00:08:38 -0600

    Updates

---
+++ precache-manifest.edc60159d25d3521294a08cca172b62d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c180653f983680d4e67f6727453c2b8",
+    "url": "/myrobot/index.html"
+  },
+  {
+    "revision": "104d37ee58610c42c702",
+    "url": "/myrobot/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "82265f07478d24a59ef5",
+    "url": "/myrobot/static/css/main.271e8299.chunk.css"
+  },
+  {
+    "revision": "104d37ee58610c42c702",
+    "url": "/myrobot/static/js/2.1c442eba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myrobot/static/js/2.1c442eba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82265f07478d24a59ef5",
+    "url": "/myrobot/static/js/main.8d403ba0.chunk.js"
+  },
+  {
+    "revision": "6126a1cc9631cb0725e3",
+    "url": "/myrobot/static/js/runtime-main.ad0d172a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/myrobot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
